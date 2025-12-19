27fd05a6f0fa2c3adf308213588b53aff54e5a09
Prasoon Mahawar <prasoonmahawar21@gmail.com>
2025-12-19 22:26:08 +0530

    first solana contract in Rust

---
+++ kiratCohort3/sol-counter-program/src/lib.rs
@@ -0,0 +1,33 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+    entrypoint
+}
+enum Instruction{
+    Increment(u32),
+    Decrement(u32)
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter{
+    count: u32,
+}
+entrypoint!(process_instruction);
+ pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+    let mut counter = Counter::try_from_slice(&acc.data.borrow())?;
+    let instruction = Instruction::try_from_slice(instruction_data)?;
+    match instruction {
+        Instruction::Increment(value) => counter.count += value,
+        Instruction::Decrement(value) => counter.count -= value,
+    }
+    counter.serialize(&mut *acc.data.borrow_mut())?;
+    Ok(())
+}
